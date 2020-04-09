f2a1790d213c30f2993530037a6e8c1fd430c4ea
JeremySenn01 <jeremy.senn20801@gmail.com>
2020-04-09 14:55:35 +0200

    Generated empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpendingsComponent } from './spendings/spendings.component';
+import { NewSpendingComponent } from './new-spending/new-spending.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpendingsComponent,
+    NewSpendingComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule
